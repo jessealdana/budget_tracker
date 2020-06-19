5740938ff9883420cd1a14a899d2eb3c87f4a52b
Jesse Aldana <jesse.aldana@gmail.com>
2020-06-19 08:51:47 -0700

    fixed offline functionality and added readme skeleton

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # budget_tracker
+
+  ![Mozilla Add-on](https://img.shields.io/amo/v/blue?color=blue&label=Budget%20Tracker&logoColor=white)
+  
+ ## Description
+  
+
+ ## User Story
+
+
+ 
+ ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#license)
+  * [Credits](#credits)
+  * [Tests](#tests)
+  * [Questions](#questions)
+ ## Installation
+
+ This app is run from the Heroku link: 
+
+
+
+
+
+ ## Usage
+ 
+
+
+ ## License
+ 
+ This App is licensed under the MIT license.
+
+ ## Credits
+ 
+ Jesse Aldana
+
+ ## Tests
+ 
+ There are no tests with this app.
+
+ ## Questions?
+ 
+ Contact jessealdana at Github.
+ 
+ ![Image of user](https://avatars0.githubusercontent.com/u/61436744?v=4)
--- public/db.js
+++ public/db.js
@@ -4,13 +4,12 @@ const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
   // create object store called "pending" and set autoIncrement to true
-db.createObjectStore("pending", {
-  autoIncrement: true
-})
+const db = event.target.result;
+db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
-  db = target.result;
+  db = event.target.result;
 
   if (navigator.onLine) {
     checkDatabase();
@@ -18,26 +17,25 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  // log error here
   console.log(event);
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
-  const transaction = db.transaction(["pending"], "readWrite");
-  // access your pending object store
-  const pendingStore = transaction.objectStore("pending");
-  // add record to your store with add method.
-  pendingStore.add(record)
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access the pending object store
+  const store = transaction.objectStore("pending");
+  // add record to the store with add method.
+  store.add(record)
 }
 
 function checkDatabase() {
   // open a transaction on your pending db
-  const transaction = db.transaction(["pending"], "readWrite");
-  // access your pending object store
-  const pendingStore = transaction.objectStore("pending");
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access the pending object store
+  const store = transaction.objectStore("pending");
   // get all records from store and set to a variable
-  pendingStore.getAll()
+  const getAll = store.getAll()
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
@@ -51,11 +49,11 @@ function checkDatabase() {
       })
       .then(response => response.json())
       .then(() => {
-          // if successful, open a transaction on your pending db
-          const transaction = db.transaction(["pending"], "readWrite");
-          // access your pending object store
+          // if successful, open a transaction on the pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+          // access thepending object store
           const pendingStore = transaction.objectStore("pending");
-          // clear all items in your store
+          // clear all items in the store
           pendingStore.clear();
       });
     }
